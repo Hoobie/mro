96c755af8b630ce59a147a997295dde3f34da6a8
hoobie <slojewsk@student.agh.edu.pl>
2015-10-11 20:10:06 +0200

    Complete 1-nn method

--- nn1.py
+++ nn1.py
@@ -1,23 +1,87 @@
+import copy
 import gzip
 import cPickle
 import sys
+import random
+import time
 
-f = gzip.open('mnist.pkl.gz', 'rb')
-train_set, valid_set, test_set = cPickle.load(f)
-f.close()
+import matplotlib.pyplot as plt
 
-train_x, train_y = train_set
-test_x, test_y = test_set
+import numpy
 
-for test_vector, test_digit in zip(test_x, test_y)[0:50]:
-    nearest = (sys.maxint, [], [])
-    for train_vector, train_digit in zip(train_x, train_y)[0:500]:
-        dist = 0
-        for test_xi, train_xi in zip(test_vector, train_vector):
-            dist += pow(train_xi - test_xi, 2)
-        if dist < nearest[0]:
-            nearest = (dist, test_vector, train_digit)
-    if nearest[2] == test_digit:
-        print "OK"
-    else:
-        print "ERROR"
+SAMPLE_COUNT = 10000
+
+
+def count_distance(vector1, vector2):
+    dist = 0
+    for x1, x2 in zip(vector1, vector2):
+        dist += pow(x2 - x1, 2)
+    return dist
+
+
+def implementation1():
+    error_count = 0
+    for test_vector, test_digit in zip(test_x_copy, test_y):
+        nearest = (sys.maxint, [], [])
+        for train_vector, train_digit in zip(train_x_copy, train_y):
+            distance = count_distance(test_vector, train_vector)
+            if distance < nearest[0]:
+                nearest = (distance, test_vector, train_digit)
+        if nearest[2] != test_digit:
+            error_count += 1
+    return error_count
+
+
+def implementation2():
+    error_count = 0
+    for test_vector, test_digit in zip(test_x_copy, test_y):
+        subtracting = train_x_copy - test_vector
+        powered = subtracting ** 2
+        summed = numpy.sum(powered, axis=1)
+        min_index = numpy.argmin(summed)
+        if test_digit != train_y[min_index]:
+            error_count += 1
+    return error_count
+
+
+def random_vector_scale(vector, scale):
+    index = random.randint(0, 728)
+    for x in vector:
+        x[index] *= scale
+    return vector
+
+
+if __name__ == "__main__":
+    f = gzip.open('mnist.pkl.gz', 'rb')
+    train_set, valid_set, test_set = cPickle.load(f)
+    f.close()
+
+    train_x, train_y = train_set
+    test_x, test_y = test_set
+    train_y = train_y[0:SAMPLE_COUNT]
+    test_y = test_y[0:SAMPLE_COUNT]
+
+    errors = []
+
+    #s_range = [1]
+    s_range = xrange(0, 21)
+    for s in s_range:
+        train_x_copy = random_vector_scale(copy.deepcopy(train_x[0:SAMPLE_COUNT]), s)
+        test_x_copy = random_vector_scale(copy.deepcopy(test_x[0:SAMPLE_COUNT]), s)
+
+        start_time = time.time()
+
+        print "\ns:", s
+
+        error_percentage = implementation2() * 100 / SAMPLE_COUNT
+        errors.append(error_percentage)
+        print "Error percentage:", error_percentage, "%"
+
+        elapsed_time = time.time() - start_time
+        print "Elapsed time:", elapsed_time, "s"
+
+    plt.plot(s_range, errors, label="error percentage")
+    plt.xlabel("s")
+    plt.ylabel("errors [%]")
+    plt.legend()
+    plt.show()
